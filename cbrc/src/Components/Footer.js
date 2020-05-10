ca7fc6c97a8247b68a0343c4f70a4fe4ccd99915
MuteBard <csevere@brandeis.edu>
2020-05-10 01:34:34 -0400

    added tables to replace cards for user search

--- cbrc/src/Components/Footer.js
+++ cbrc/src/Components/Footer.js
@@ -12,8 +12,8 @@ export default function Footer(){
   return(
     <div className = "Footer">
       <Row justify="space-around" align="middle">
-        <Col span={2} className="iconText" offset={20}>Tweet Issues</Col>
-        <Col span={2} className="iconText" >Donate</Col>
+        <Col span={2} className="iconText" offset={20}>Issues?</Col>
+        <Col span={2} className="iconText">Donate</Col>
       </Row>
       <Row>
         <Col span={2} offset={20}><TwitterOutlined className="icon" style={{fontSize:"3em", color:"#4AE3B5"}}/></Col>
